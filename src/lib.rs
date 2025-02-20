6db6f2d6e3bc4ccce69976d12fd98c6eedf1957f
Maxi Saparov <maxi.saparov@gmail.com>
2025-02-19 19:08:16 -0500

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,131 @@
+use std::{
+    ptr::null_mut,
+    sync::atomic::{AtomicPtr, Ordering},
+    task::Waker,
+};
+
+#[cfg(feature = "cache-padded")]
+use crossbeam_utils::CachePadded;
+
+#[cfg(feature = "cache-padded")]
+pub struct WakerQueue {
+    head: CachePadded<AtomicPtr<WakerNode>>,
+    tail: CachePadded<AtomicPtr<WakerNode>>,
+}
+
+#[cfg(not(feature = "cache-padded"))]
+pub struct WakerQueue {
+    head: AtomicPtr<WakerNode>,
+    tail: AtomicPtr<WakerNode>,
+}
+
+impl Drop for WakerQueue {
+    fn drop(&mut self) {
+        let mut head = self.head.swap(null_mut(), Ordering::SeqCst);
+        let tail = self.tail.load(Ordering::SeqCst);
+
+        while head != tail {
+            if head.is_null() {
+                unreachable!("failed to deallocate WakerQueue");
+            }
+
+            let tmp = unsafe { Box::from_raw(head) };
+            head = tmp.next.swap(null_mut(), Ordering::SeqCst);
+            drop(tmp);
+        }
+
+        if !head.is_null() {
+            unsafe { drop(Box::from_raw(head)) };
+        }
+    }
+}
+
+struct WakerNode {
+    next: AtomicPtr<WakerNode>,
+    waker: Option<Waker>,
+}
+
+impl Drop for WakerNode {
+    fn drop(&mut self) {
+        let ptr = self.next.swap(null_mut(), Ordering::SeqCst);
+        if !ptr.is_null() {
+            unsafe { drop(Box::from_raw(ptr)) };
+        }
+    }
+}
+
+impl WakerQueue {
+    #[cfg(feature = "cache-padded")]
+    pub const fn new() -> Self {
+        WakerQueue {
+            head: CachePadded::new(AtomicPtr::new(null_mut::<WakerNode>())),
+            tail: CachePadded::new(AtomicPtr::new(null_mut::<WakerNode>())),
+        }
+    }
+
+    #[cfg(not(feature = "cache-padded"))]
+    pub const fn new() -> Self {
+        WakerQueue {
+            head: AtomicPtr::new(null_mut::<WakerNode>()),
+            tail: AtomicPtr::new(null_mut::<WakerNode>()),
+        }
+    }
+
+    pub fn register(&self, waker: Waker) {
+        let node = Box::into_raw(Box::new(WakerNode {
+            next: AtomicPtr::new(null_mut()),
+            waker: Some(waker),
+        }));
+
+        let prev_tail = self.tail.swap(node, Ordering::Relaxed);
+
+        unsafe {
+            match prev_tail.as_mut() {
+                Some(prev) => prev.next.store(node, Ordering::Relaxed),
+                None => self.head.store(node, Ordering::Relaxed),
+            }
+        }
+    }
+
+    pub fn wake_all(&self) {
+        let tail = self
+            .tail
+            .swap(null_mut::<WakerNode>().into(), Ordering::Relaxed);
+
+        // tail being null implies nothing has been pushed into the queue
+        if tail.is_null() {
+            return;
+        }
+
+        let mut head = self
+            .head
+            .swap(null_mut::<WakerNode>().into(), Ordering::Relaxed);
+
+        // if tail isn't null we are just waiting for a register
+        // to finish setting the head
+        while head.is_null() {
+            head = self
+                .head
+                .swap(null_mut::<WakerNode>().into(), Ordering::Relaxed);
+        }
+
+        // safety: we know head isn't null from above
+        let mut head = unsafe { Box::from_raw(head) };
+        head.waker.take().map(|w| w.wake());
+
+        while head.as_ref() as *const _ != tail {
+            head = loop {
+                let next = head.next.load(Ordering::Relaxed);
+
+                if next.is_null() {
+                    std::hint::spin_loop();
+                    continue;
+                }
+
+                break unsafe { Box::from_raw(next) };
+            };
+
+            head.waker.take().map(|w| w.wake());
+        }
+    }
+}
